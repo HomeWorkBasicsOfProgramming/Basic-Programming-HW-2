adf5a7b8b8ff79b3e1d97e8209055ef7f15088e7
ilbeyataberkcan <32396853+ilbeyataberkcan@users.noreply.github.com>
2017-11-30 18:36:17 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/RangedWeapon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class RangedWeapon: Weapon
+    {
+        public string name;
+        public string weaponType;
+
+        public RangedWeapon(string name, string weaponType): base (name)
+        {
+            this.name = name;
+            this.weaponType = weaponType;
+        }
+
+       static string bulletType()
+        {
+            if (weaponType == "LASER RIFLE")
+            {
+
+            }
+            else if (weaponType == "GAUSS RIFLE")
+            {
+
+            }
+        }
+       
+        
+       
+            
+    }
+}
